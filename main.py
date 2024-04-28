3fb17f1958f0ea565b6f5fb8c9338ca2cf1e967b
Mateusz Kuczynski <mateuszkuczynskianalytics@gmail.com>
2024-04-28 17:47:21 +0200

    Added task number 3

---
+++ main.py
@@ -0,0 +1,20 @@
+def licz_sume(input_dict):
+
+    total_knuts = sum(input_dict.get("knut", []))
+    total_sykls = sum(input_dict.get("sykl", []))
+    total_galeons = sum(input_dict.get("galeon", []))
+    
+    total_sykls += total_knuts // 21
+    remaining_knuts = total_knuts % 21
+    
+    total_galeons += total_sykls // 17
+    remaining_sykls = total_sykls % 17
+    
+    output_dict = {
+        "galeon": total_galeons,
+        "sykl": remaining_sykls,
+        "knut": remaining_knuts
+    }
+    
+    return output_dict
+
