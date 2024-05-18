187ba9151fad8addfdab59d52372b6ec160eb5b3
Natalia.Dziwisz <nataliadziwisz13@gmail.com>
2024-05-18 17:13:57 +0200

    README update

---
+++ Natalia_Zadanie_5/Natalia_Zadanie5.py
@@ -0,0 +1,13 @@
+def waluta_dict_na_str(waluta):
+    wynik = ""
+    if "galeon" in waluta and waluta["galeon"] != 0:
+        wynik += str(waluta["galeon"]) + " galeon "
+    if "sykl" in waluta and waluta["sykl"] != 0:
+        wynik += str(waluta["sykl"]) + " sykl "
+    if "knut" in waluta and waluta["knut"] != 0:
+        wynik += str(waluta["knut"]) + " knut"
+    return wynik.strip()
+
+# Przykładowe wywołania funkcji
+print(waluta_dict_na_str({"galeon": 0, "sykl": 0, "knut": 13}))
+print(waluta_dict_na_str({"galeon": 17, "sykl": 2, "knut": 13}))
