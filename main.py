eb35ab4dfc9a7aec9e32caf7a5b2d7ae74ea869e
krzychpoznan <82968@student.ue.poznan.pl>
2024-05-20 23:47:55 +0200

    zaktualizowano zmiany

--- main.py
+++ main.py
@@ -125,6 +125,44 @@ def waluta_dict_na_str(waluta):
         wynik += str(waluta["knut"]) + " knut"
     return wynik.strip()
 
+# Zadanie 6
+
+def waluta_str_na_dict(ciag_znakow):
+#OPIS
+#Funkcja przetwarza ciąg znaków reprezentujący ceny w różnych walutach na słownik.
+#Parametry: ciag_znakow (str): Ciąg znaków zawierający ceny w formacie "wartość waluta", gdzie wartość jest liczbą całkowitą, a waluta może być galeonem (g), syklem (s) lub knutem (k).
+#Zwraca: dict: Słownik zawierający ceny w formacie {"galeon": wartość, "sykl": wartość, "knut": wartość}. Domyślnie wartości dla każdej waluty są ustawione na 0, chyba że występują w ciągu znaków.
+
+    # Inicjalizacja słownika
+    waluta_dict = {"galeon": 0, "sykl": 0, "knut": 0}
+
+    # Podział ciągu po spacji
+    elements = ciag_znakow.split()
+
+    # Iteracja po elementach co drugim
+    for i in range(0, len(elements), 2):
+        # Ustalenie klucza i wartości
+        klucz = elements[i + 1]
+        wartosc = int(elements[i]) if elements[i].isdigit() else 0
+
+        # Zmiana wartości na kluczach odpowiednich bilonów
+        if klucz.startswith("g"):
+            waluta_dict["galeon"] = wartosc
+        elif klucz.startswith("s"):
+            waluta_dict["sykl"] = wartosc
+        elif klucz.startswith("k"):
+            waluta_dict["knut"] = wartosc
+
+    return waluta_dict
+
+
+# Testowanie funkcji
+print(waluta_str_na_dict("13 knut"))
+print(waluta_str_na_dict("17 galeon 2 sykl 13 knut"))
+print(waluta_str_na_dict("28 sykl"))
+print(waluta_str_na_dict("12 galeon 3 knut"))
+
+
 
 #Zadanie 7
 
