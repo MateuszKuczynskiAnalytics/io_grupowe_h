e0c1a1d520dcfc931e3946a2b9d9629ae8e00ec2
Wiktor Musiałowski <93775@student.ue.poznan.pl>
2024-06-07 14:57:01 +0200

    poprawa kodu w pliku test_zad_6.py

--- test_zad_6.py
+++ test_zad_6.py
@@ -1,29 +1,26 @@
 import unittest
 from main import waluta_str_na_dict
 
-
 class TestWalutaStrNaDict(unittest.TestCase):
+    def test_waluta_str_na_dict_single_currency(self):
+        result = waluta_str_na_dict("13 knut")
+        self.assertEqual(result, {"galeon": 0, "sykl": 0, "knut": 13})
 
-    def test_only_knuts(self):
-        ciag_znakow = "13 knut"
-        expected_output = {"galeon": 0, "sykl": 0, "knut": 13}
-        self.assertEqual(waluta_str_na_dict(ciag_znakow), expected_output)
+    def test_waluta_str_na_dict_multiple_currencies(self):
+        result = waluta_str_na_dict("17 galeon 2 sykl 13 knut")
+        self.assertEqual(result, {"galeon": 17, "sykl": 2, "knut": 13})
 
-    def test_all_non_zero(self):
-        ciag_znakow = "17 galeon 2 sykl 13 knut"
-        expected_output = {"galeon": 17, "sykl": 2, "knut": 13}
-        self.assertEqual(waluta_str_na_dict(ciag_znakow), expected_output)
+    def test_waluta_str_na_dict_missing_currency(self):
+        result = waluta_str_na_dict("28 sykl")
+        self.assertEqual(result, {"galeon": 0, "sykl": 28, "knut": 0})
 
-    def test_no_sykls(self):
-        ciag_znakow = "28 sykl"
-        expected_output = {"galeon": 0, "sykl": 28, "knut": 0}
-        self.assertEqual(waluta_str_na_dict(ciag_znakow), expected_output)
+    def test_waluta_str_na_dict_empty_input(self):
+        result = waluta_str_na_dict("")
+        self.assertEqual(result, {"galeon": 0, "sykl": 0, "knut": 0})
 
-    def test_no_galeons(self):
-        ciag_znakow = "12 galeon 3 knut"
-        expected_output = {"galeon": 12, "sykl": 0, "knut": 3}
-        self.assertEqual(waluta_str_na_dict(ciag_znakow), expected_output)
+    def test_waluta_str_na_dict_invalid_input(self):
+        result = waluta_str_na_dict("12 galeon 3,5 knut")
+        self.assertEqual(result, {"galeon": 12, "sykl": 0, "knut": 0})
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
